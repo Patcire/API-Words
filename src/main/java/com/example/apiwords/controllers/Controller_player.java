694979c0a4dc846a913fe4ca48d245b2d94f1ad0
Patricio <pcifrey501@g.educaand.es>
2023-12-08 20:56:03 +0100

    controller packaged created

---
+++ src/main/java/com/example/apiwords/controllers/Controller_player.java
@@ -0,0 +1,11 @@
+package com.example.apiwords.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller_player {
+
+
+}
